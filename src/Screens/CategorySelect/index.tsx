0e5cb6231b563ef5da63af6bbbfc74cf78c524e3
EngJao89 <eng_joao@outlook.com>
2022-05-19 18:31:42 -0300

    Closes 09: "Implementa React Form Hook."

--- src/Screens/CategorySelect/index.tsx
+++ src/Screens/CategorySelect/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import { Button} from '../../components/Form/Button'
+import { categories } from '../../utils/categories'
 import { 
   Container, 
   Header, 
-  Title
+  Title,
+  Category,
+  Icon,
+  Name,
+  Separator,
+  Footer
 } from './styles';
 
 interface Category {
@@ -11,19 +19,49 @@ interface Category {
 }
 
 interface Props {
-  category: string;
-  setCategory: (name: string) => void;
+  category: Category;
+  setCategory: (category: Category) => void;
   closeSelectCategory: () => void;
 }
 
 export function CategorySelect(
-  { category, setCategory, closeSelectCategory }: Props
-) {
+  { 
+    category, 
+    setCategory, 
+    closeSelectCategory 
+  }: Props) {
+
+    function handleCategorySelect(category: Category){
+      setCategory(category);
+    }
+
   return (
     <Container>
       <Header>
         <Title>Categoria</Title>
       </Header>
+      <FlatList 
+        data={categories}
+        style={{ flex: 1, width: '100%' }}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Category 
+            onPress={() => handleCategorySelect(item)}
+            isActive={category.key === item.key}
+          >
+            <Icon name={item.icon} size={20} color={item.color} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+
+      <Footer>
+        <Button 
+          title="Selecionar" 
+          onPress={closeSelectCategory}
+        />
+      </Footer>
     </Container>
   )
 }
