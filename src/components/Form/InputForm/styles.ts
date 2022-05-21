9f5e1b1dea417b634f3ea9cd805e018408fec784
EngJao89 <eng_joao@outlook.com>
2022-05-21 00:00:02 -0300

    Closes 10: "Validação de campos."

--- src/components/Form/InputForm/styles.ts
+++ src/components/Form/InputForm/styles.ts
@@ -4,3 +4,10 @@ import { RFValue } from 'react-native-responsive-fontsize';
 export const Container = styled.View`
   width: 100%;
 `;
+
+export const Error = styled.View`
+  color: ${({ theme }) => theme.colors.attention};
+  font-size: ${RFValue(14)};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  margin: 7px 0;
+`;
