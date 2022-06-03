16af97bd3c4337493f85f36b35f9ca2a35bf616a
EngJao89 <eng_joao@outlook.com>
2022-06-03 18:18:52 -0300

    Closes 20: "Refatora resumo por categoria."

--- src/components/TransactionCard/styles.ts
+++ src/components/TransactionCard/styles.ts
@@ -3,11 +3,11 @@ import { Feather } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 interface TransactionProps {
-  type: 'income' | 'outcome';
+  type: 'positive' | 'negative';
 }
 
 export const Container = styled.View`
-  background: ${({ theme }) => theme.colors.shape};
+  background-color: ${({ theme }) => theme.colors.shape};
   border-radius: 5px;
   padding: 17px 24px;
   margin-bottom: 16px;
@@ -19,17 +19,17 @@ export const Title = styled.Text`
 `;
 
 export const Amount = styled.Text<TransactionProps>`
+  font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(20)}px;
-  margin-top: 2px;
-  color: ${({ theme, type }) => 
+  color: ${({ theme, type }) =>
   type === 'positive' ? theme.colors.success : theme.colors.attention};
+  margin-top: 2px;
 `;
 
 export const Footer = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-
   margin-top: 19px;
 `;
 
@@ -53,5 +53,3 @@ export const Date = styled.Text`
   font-size: ${RFValue(14)}px;
   color: ${({ theme }) => theme.colors.text};
 `;
-
-export const TransactionList  = styled.FlatList``;
