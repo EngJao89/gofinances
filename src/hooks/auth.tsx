cdfc3888f2f229a3af86909603e2e22a2de8a0ba
EngJao89 <eng_joao@outlook.com>
2022-09-28 01:11:39 -0300

    Closes 39:"Melhorar login e logout do app."

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -28,6 +28,8 @@ interface IAuthContextData {
   user: User;
   signInWithGoogle(): Promise<void>;
   signInWithApple(): Promise<void>;
+  signOut(): Promise<void>;
+  userStorageLoading: boolean;
 }
 
 interface AuthorizationResponse {
@@ -85,11 +87,14 @@ function AuthProvider({ children }: AuthProviderProps ) {
       });
 
       if (credential) {
+        const name = credential.fullName!.givenName!;
+        const photo = `https://ui-avatars.com/api/?name=${name}&length=1`;
+
         const userLogged = {
           id: String(credential.user),
           email: credential.email!,
-          name: credential.fullName!.givenName!,
-          photo: undefined
+          name,
+          photo,
         };
 
         setUser(userLogged);
@@ -114,13 +119,22 @@ function AuthProvider({ children }: AuthProviderProps ) {
 
     loadUserStorageDate();
   },[]);
-  
+
+  async function signOut(){
+    setUser({} as User);
+    await AsyncStorage.removeItem(userStorageKey);
+  }
+
+  useEffect(() => {
+  },[]);
 
   return (
     <AuthContext.Provider value={{
       user,
       signInWithGoogle,
-      signInWithApple
+      signInWithApple,
+      signOut,
+      userStorageLoading
     }}>
       {children}
     </AuthContext.Provider>
