839c1ba8c60420a7b8b5f09dd8369f278c80bb74
EngJao89 <eng_joao@outlook.com>
2022-11-30 15:08:54 -0300

    Closes 57:"Implementa testes com mocks personalizados em auth."

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,32 +1,22 @@
-import React from 'react';
-
-import { render } from '@testing-library/react-native';
+import { mocked } from 'ts-jest/utils';
 import { renderHook, act } from '@testing-library/react-native';
 import { AuthProvider, useAuth } from './auth';
-import { startAsync } from 'expo-auth-session';
+import { logInAsync } from 'expo-google-app-auth';
 
-jest.mock('expo-auth-session', () => {
-  return {
-    startAsync: () => {
-      return {
-        type: 'sucess',
-        params: {
-          access_token: 'google-token'
-        }
-      }
-    }
-  }
-});
+jest.mock('expo-auth-session');
 
 describe('Auth Hook', () =>{
-  it('should be able to sign in with Google account existing', async () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      id: `userInfo.id`,
-      email: `userInfo.email`,
-      photo: `userInfo.given_name`,
-      locale: `userInfo.locale`,
-      verified_email: `userInfo.verified_email`
-    })) as jest.Mock;
+  it('should able to sign in with Google account existing', async () => {
+    const googleMocked = mocked(logInAsync as any);
+    googleMocked.mockReturnValue({
+      type: 'success',
+      user: {
+        id: 'any_id',
+        email: 's4bo4gedtn@gmail.com',
+        name: 'João Ricardo',
+        photo: 'any_photo.png'
+      }
+    });
 
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
@@ -34,6 +24,28 @@ describe('Auth Hook', () =>{
 
     await act(() => result.current.signInWithGoogle());
 
-    expect(result.current.user).toBeTruthy();
+    expect(result.current.user.email)
+    .toBe('s4bo4gedtn@gmail.com');
+  });
+
+  it('user should be not connect if cancel authentication with Google', async () => {
+    const googleMocked = mocked(logInAsync);
+    googleMocked.mockReturnValue({
+      type: 'success',
+      user: {
+        id: 'any_id',
+        email: 's4bo4gedtn@gmail.com',
+        name: 'João Ricardo',
+        photo: 'any_photo.png'
+      }
+    });
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user)
+    .toHaveProperty('id');
   });
 });
