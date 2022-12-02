23b4d14082372e48ceb78de95d03eb05dc330ac7
EngJao89 <eng_joao@outlook.com>
2022-12-01 23:36:41 -0300

    Closes 58:"Implementa teste de eventos."

--- src/components/Form/CategorySelectButton/index.tsx
+++ src/components/Form/CategorySelectButton/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
 import {
   Container,
@@ -6,14 +7,15 @@ import {
   Icon,
 } from './styles';
 
-interface Props {
+interface Props extends RectButtonProps {
   title: string;
   onPress: () => void;
 }
 
 export function CategorySelectButton({
   title,
-  onPress
+  onPress,
+  testID="button-category"
 }: Props){
   return(
     <Container onPress={onPress}>
